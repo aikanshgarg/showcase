916d5e63274c483901e1959034b6d70e81f80100
root <aikanshgarg0303@gmail.com>
2018-12-14 17:52:16 +0530

    add active class for hover on mobile

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+//When the user scrolls the page, execute myFunction 
+	window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+	function myFunction() {
+	  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	  var scrolled = (winScroll / height) * 100;
+	  document.getElementById("myBar").style.width = scrolled + "%";
+	}
+
+
+
+
+// var myVar;
+
+// function myFunction() {
+//   myVar = setTimeout(showPage, 5000);
+// }
+
+// function showPage() {
+
+// 	 // When the user scrolls the page, execute myFunction 
+// 	window.onscroll = function() {myFunction()};
+
+// 	function myFunction() {
+// 	  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+// 	  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+// 	  var scrolled = (winScroll / height) * 100;
+// 	  document.getElementById("myBar").style.width = scrolled + "%";
+// 	}
+
+
+//   document.getElementById("loader").style.display = "none";
+//   document.getElementById("myDiv").style.display = "block";
+// }
